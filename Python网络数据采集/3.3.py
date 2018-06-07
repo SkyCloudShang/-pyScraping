a4b8d70fc26c99d94af54ed44df3a80ddea6ac53
SkyCloudShang <814359194@qq.com>
2018-06-07 09:50:12 +0800

    添加第5章数据存储小节

--- Python网络数据采集/3.3.py
+++ Python网络数据采集/3.3.py
@@ -9,3 +9,10 @@ pages=set()
 random.seed(datetime.datetime.now())
 
 #获取页面所有内链的列表
+def getInternalLinks(bsObj,includeUrl):
+    includeUrl=urlparse(includeUrl).scheme+"://"+urlparse(includeUrl).netloc
+    internalLinks=[]
+    for link in bsObj.findAll("a",href=re.compile("^(/|.*"+includeUrl+")")):
+        if link.attrs['href'] not in internalLinks:
+
+#未完成 待续
